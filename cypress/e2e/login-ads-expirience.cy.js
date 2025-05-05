e2ee2bdf1a74758537c7571c1a367b34ffcd97f1
Fagundes Fernandes Ferreira <idd_fferreira@uolinc.com>
2025-05-05 15:10:48 -0300

    Adiciona testes para login

--- cypress/e2e/login-ads-expirience.cy.js
+++ cypress/e2e/login-ads-expirience.cy.js
@@ -1,7 +1,26 @@
 describe('Testa login da página do ads experience', () => {
 
-    it.only('deve consultar a ágina do google', () =>{
-        cy.visit('https://www.google.com.br/')
+    beforeEach (() => {
+        cy.visit('./final_app/login.html')
+      })
+    
+    
+    it('deve realizar o login com sucesso', () =>{
+        cy.get('#login').type('admin')
+        cy.get('#password').type('admin')
+        cy.contains('button', 'Login').click()
+
+        cy.get('#mensagem')
+          .should('be.visible')
+          .and('have.text', 'Login com sucesso! Redirecionando...');
+    })
+
+    it('deve exibir uma mensagem não permitindo criar outra conta', () =>{
+        cy.contains('button', 'Criar Conta').click()
+
+        cy.get('#mensagem-erro')
+           .should('be.visible')
+           .and('have.text', 'No momento não é permitido criar outra conta');
     })
 
 })
